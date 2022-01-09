5726545a33c56d4fb5b995cd4938e75ac6ef1ba8
Dhimas <dhimas.ra48@gmail.com>
2022-01-09 21:29:38 +0700

    Add project files.

---
+++ ServerChatApp_20190140112_DhimasRizqiAkbar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_20190140112;
+
+namespace ServerChatApp_20190140112_DhimasRizqiAkbar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+                Console.WriteLine("Rede ngab!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
